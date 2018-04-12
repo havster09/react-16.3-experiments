3a1eeaa0b7c7d6d2d38a6b0e247e5df75eb7223d
havster09 <havster09@gmail.com>
2018-04-12 22:10:31 +1000

    init

--- src/App.js
+++ src/App.js
@@ -7,19 +7,26 @@ const ApplicationContext = createContext();
 
 const { Provider, Consumer } = ApplicationContext;
 
-const initialState = {
-  users: ['Theo', 'Rick', 'James', 'Jimmy']
+let initialState = {
+  users: ['Theo', 'Rick', 'James', 'haven']
 };
 
-
-
-
 class App extends Component {
+  state = initialState;
   render() {
     return (
-      <Provider value={initialState}>
+      <Provider value={this.state}>
         <Header />
         <Footer />
+        <button
+          onClick={() =>
+            this.setState({
+              ...this.state,
+              ...{ users: [...this.state.users, ...['fu']] }
+            })}
+        >
+          Click it
+        </button>
       </Provider>
     );
   }
@@ -35,47 +42,63 @@ const withConsumer = BaseComponent => {
         <Consumer>
           {context => {
             return (
+              <React.Fragment>
                 <BaseComponent {...context} {...this.props} {...this.state} />
-            )
-          }
-        }       
+              </React.Fragment>
+            );
+          }}
         </Consumer>
-      )
+      );
     }
-
-  }
-}
-
+  };
+};
 
 const componentFromUI = (ui, name) => {
   return class extends Component {
+    constructor(props) {
+      super();
+    }
 
+    componentDidMount() {
+
+    }
+
+    state = { a: 1 };
     static displayName = name;
+    static getDerivedStateFromProps(nextProps, prevState) {
+      console.log(nextProps, prevState);
+      return { poo: 1, wee: 2, ...nextProps };
+    }
 
     render() {
       console.log(55, this.props.users);
       return (
         <div>
+          {JSON.stringify(this.state, null, 4)}
           {ui(this.props.users)}
         </div>
-      )
+      );
     }
+  };
+};
 
-
-  }
-}
-
-const Header = withConsumer(componentFromUI((users) => {
-  return (
-    <header>{JSON.stringify(users)} I am the header</header>
-  )
-}, 'Header'));
-const Footer = withConsumer(componentFromUI((users) => {
-  return (
-    <footer>{JSON.stringify(users)} I am the header</footer>
-  )
-}, 'Footer'));
-
-
+const Header = withConsumer(
+  componentFromUI(users => {
+    return (
+      <header>
+        {JSON.stringify(users)} I am the header
+      </header>
+    );
+  }, 'Header')
+);
+const Footer = withConsumer(
+  componentFromUI(users => {
+    return (
+      <footer>
+        {JSON.stringify(users)} I am the header
+      </footer>
+    );
+  }, 'Footer')
+);
 
 export default App;
